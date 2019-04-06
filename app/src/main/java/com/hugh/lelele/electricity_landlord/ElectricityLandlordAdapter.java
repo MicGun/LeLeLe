94bc9abf2e52b441fa824173ae611a7655fbdd95
MicGun <n1553330708@yahoo.com.tw>
2019-04-06 20:45:06 +0800

    fix imagebutton cannot be cast to button issue

--- app/src/main/java/com/hugh/lelele/electricity_landlord/ElectricityLandlordAdapter.java
+++ app/src/main/java/com/hugh/lelele/electricity_landlord/ElectricityLandlordAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.hugh.lelele.R;
@@ -30,7 +31,7 @@ public class ElectricityLandlordAdapter extends RecyclerView.Adapter {
         TextView roomNumber;
         TextView scaleLast;
         EditText scaleThis;
-        Button cleanButton;
+        ImageButton cleanButton;
 
         public ElectricityEditorLandlordItemViewHolder(@NonNull View itemView) {
             super(itemView);

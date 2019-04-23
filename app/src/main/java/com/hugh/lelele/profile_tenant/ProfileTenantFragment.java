0753068cc5bdade6f20f6f0810b6223b30e2c40f
MicGun <n1553330708@yahoo.com.tw>
2019-04-23 16:59:44 +0800

    setup profile edit modle

--- app/src/main/java/com/hugh/lelele/profile_tenant/ProfileTenantFragment.java
+++ app/src/main/java/com/hugh/lelele/profile_tenant/ProfileTenantFragment.java
@@ -3,7 +3,9 @@ package com.hugh.lelele.profile_tenant;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.text.method.KeyListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,7 @@ import com.hugh.lelele.util.ImageManager;
 import com.hugh.lelele.util.UserManager;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.hugh.lelele.R.drawable.ic_action_check;
 
 public class ProfileTenantFragment extends Fragment implements ProfileTenantContract.View {
 
@@ -28,13 +31,20 @@ public class ProfileTenantFragment extends Fragment implements ProfileTenantCont
     private EditText mCellphoneNumberEditText;
     private EditText mEmailEditText;
     private EditText mIdCardNumberEditText;
+    private EditText mGroupNameEditText;
+    private EditText mRoomNumberEditText;
+    private EditText mLandlordEmailEditText;
     private ImageView mProfileBackground;
     private ImageView mUserPicture;
 
+    private FloatingActionButton mFloatingProfileEditButton;
+
     private Tenant mTenant;
+    private boolean mIsEditable;
 
     public ProfileTenantFragment() {
         mTenant = UserManager.getInstance().getTenant();
+        mIsEditable = false;
     }
 
     @Nullable
@@ -48,14 +58,61 @@ public class ProfileTenantFragment extends Fragment implements ProfileTenantCont
                 getResources().getDimensionPixelSize(R.dimen.radius_profile_avatar)));
 
         mUserNameEditText = root.findViewById(R.id.edit_text_user_name_profile);
-        mAddressEditText = root.findViewById(R.id.edit_text_address_profile);
-        mCellphoneNumberEditText = root.findViewById(R.id.edit_text_cellphone_number_profile);
-        mEmailEditText = root.findViewById(R.id.edit_text_user_email_profile);
-        mIdCardNumberEditText = root.findViewById(R.id.edit_text_id_card_number_profile);
 
+        mAddressEditText = root.findViewById(R.id.edit_text_address_profile);
+        mAddressEditText.setTag(mAddressEditText.getKeyListener());
+
+        mCellphoneNumberEditText = root.findViewById(R.id.edit_text_cellphone_number_profile);
+        mCellphoneNumberEditText.setTag(mCellphoneNumberEditText.getKeyListener());
+
+        mEmailEditText = root.findViewById(R.id.edit_text_user_email_profile);
+
+        mIdCardNumberEditText = root.findViewById(R.id.edit_text_id_card_number_profile);
+        mIdCardNumberEditText.setTag(mIdCardNumberEditText.getKeyListener());
+
+        mGroupNameEditText = root.findViewById(R.id.text_view_group_name_profile);
+        mRoomNumberEditText = root.findViewById(R.id.text_view_room_number_profile);
+        mLandlordEmailEditText = root.findViewById(R.id.text_view_landlord_email_profile);
+        mFloatingProfileEditButton = root.findViewById(R.id.button_profile_edit);
+        mFloatingProfileEditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mIsEditable) {
+                    mIsEditable = !mIsEditable;
+                    setEditability();
+                    mFloatingProfileEditButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_check));
+                } else {
+                    mIsEditable = !mIsEditable;
+                    setEditability();
+                    mFloatingProfileEditButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_edit));
+                }
+            }
+        });
+
+        setEditability();
         setupTenantProfileUi();
+        setupRentalInfo();
 
         return root;
+    }
+
+    private void setEditability() {
+
+        if (mIsEditable) {
+            mAddressEditText.setKeyListener((KeyListener) mAddressEditText.getTag());
+            mCellphoneNumberEditText.setKeyListener((KeyListener) mCellphoneNumberEditText.getTag());
+            mIdCardNumberEditText.setKeyListener((KeyListener) mIdCardNumberEditText.getTag());
+        } else {
+            mUserNameEditText.setKeyListener(null);
+            mAddressEditText.setKeyListener(null);
+            mCellphoneNumberEditText.setKeyListener(null);
+            mEmailEditText.setKeyListener(null);
+            mIdCardNumberEditText.setKeyListener(null);
+            mGroupNameEditText.setKeyListener(null);
+            mRoomNumberEditText.setKeyListener(null);
+            mLandlordEmailEditText.setKeyListener(null);
+        }
+
     }
 
     private void setupTenantProfileUi() {
@@ -69,6 +126,15 @@ public class ProfileTenantFragment extends Fragment implements ProfileTenantCont
         mEmailEditText.setText(mTenant.getEmail());
         mIdCardNumberEditText.setText(mTenant.getIdCardNumber());
 
+    }
+
+    private void setupRentalInfo() {
+
+        if (mTenant.isBinding()) {
+            mGroupNameEditText.setText(mTenant.getGroup());
+            mRoomNumberEditText.setText(mTenant.getRoomNumber());
+            mLandlordEmailEditText.setText(mTenant.getLandlordEmail());
+        }
     }
 
     public static ProfileTenantFragment newInstance() {
